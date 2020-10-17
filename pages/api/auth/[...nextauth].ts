357591fe5aecd77af2c7513e91453e3add915b9f
rodzy <49137701+rodzy@users.noreply.github.com>
2020-10-17 14:17:24 -0600

    Imports adapters done ✅

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
 import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+import Adapters from "next-auth/adapters";
+
+const prisma = new PrismaClient();
 
 const options = {
     providers: [
@@ -33,15 +37,14 @@ const options = {
                 process.env.GOOGLE_SECRET !== undefined
                     ? process.env.GOOGLE_SECRET
                     : "",
-            
-        })
+        }),
     ],
     debug: process.env.NODE_ENV === "development",
     secret: process.env.AUTH_SECRET,
     jwt: {
         secret: process.env.JWT_SECRET,
     },
-
+    adapter: Adapters.Prisma.Adapter({ prisma }),
     // A database is optional, but required to persist accounts in a database
     database: process.env.DATABASE_URL,
 };
